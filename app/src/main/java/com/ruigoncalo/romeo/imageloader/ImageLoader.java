1b15149ef782d8e6c1521c54832511b7d3e21488
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2016-02-28 16:39:28 +0000

    Create ImageManager to handle image loading

--- app/src/main/java/com/ruigoncalo/romeo/imageloader/ImageLoader.java
+++ app/src/main/java/com/ruigoncalo/romeo/imageloader/ImageLoader.java
@@ -1,7 +1,13 @@
 package com.ruigoncalo.romeo.imageloader;
 
+import android.content.Context;
+import android.widget.ImageView;
+
 /**
  * Created by ruigoncalo on 28/02/16.
  */
 public interface ImageLoader {
+    void load(Context context, String urlResource, ImageView target);
+
+    void load(Context context, int resource, ImageView target);
 }
--- app/src/main/java/com/ruigoncalo/romeo/imageloader/ImageManager.java
+++ app/src/main/java/com/ruigoncalo/romeo/imageloader/ImageManager.java
@@ -1,7 +1,32 @@
 package com.ruigoncalo.romeo.imageloader;
 
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
 /**
  * Created by ruigoncalo on 28/02/16.
  */
-public class ImageManager {
+public class ImageManager implements ImageLoader {
+    private static ImageManager instance = new ImageManager();
+
+    public static ImageManager getInstance(){
+        return instance;
+    }
+
+    private ImageManager(){
+
+    }
+
+    @Override
+    public void load(Context context, String urlResource, ImageView target){
+        Picasso.with(context).load(urlResource).into(target);
+    }
+
+    @Override
+    public void load(Context context, int resource, ImageView target){
+        Picasso.with(context).load(resource).into(target);
+    }
+
 }
