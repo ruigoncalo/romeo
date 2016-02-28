b6b9e02fe58d2e0fdfbf18d9ca412bbde0e07d1b
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2016-02-28 16:45:35 +0000

    Create viewmodel for streams

--- app/src/main/java/com/ruigoncalo/romeo/ui/viewmodel/StreamViewModel.java
+++ app/src/main/java/com/ruigoncalo/romeo/ui/viewmodel/StreamViewModel.java
@@ -4,4 +4,20 @@ package com.ruigoncalo.romeo.ui.viewmodel;
  * Created by ruigoncalo on 28/02/16.
  */
 public class StreamViewModel {
+    private final String label;
+    private final String imageUrl;
+
+    public StreamViewModel(String label, String imageUrl) {
+        this.label = label;
+        this.imageUrl = imageUrl;
+    }
+
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
 }
