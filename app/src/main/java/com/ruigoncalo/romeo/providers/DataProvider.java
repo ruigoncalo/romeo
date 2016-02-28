02b818168df58ec31e4d3e9debf50ce7acb3e1d5
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2016-02-28 16:44:55 +0000

    Create data provider to fetch local data

--- app/src/main/java/com/ruigoncalo/romeo/providers/DataProvider.java
+++ app/src/main/java/com/ruigoncalo/romeo/providers/DataProvider.java
@@ -1,7 +1,34 @@
 package com.ruigoncalo.romeo.providers;
 
+import android.content.Context;
+
+import com.ruigoncalo.romeo.cache.LocalCacheManager;
+import com.ruigoncalo.romeo.model.UserAuth;
+
 /**
  * Created by ruigoncalo on 20/02/16.
  */
 public class DataProvider {
+
+    private static DataProvider ourInstance = new DataProvider();
+    private UserAuth userAuth;
+
+    public static DataProvider getInstance() {
+        return ourInstance;
+    }
+
+    private DataProvider() {
+    }
+
+    public UserAuth getUserAuth(Context context){
+        if(userAuth == null){
+            userAuth = LocalCacheManager.getInstance().getUserAuth(context);
+        }
+
+        return userAuth;
+    }
+
+    public void saveUserAuth(Context context, UserAuth userAuth){
+        LocalCacheManager.getInstance().saveUserAuth(context, userAuth);
+    }
 }
