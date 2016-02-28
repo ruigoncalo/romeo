54f48011e53bf80802b35ecc20eacb2c437c9538
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2016-02-28 16:39:59 +0000

    Create splash screen

--- app/src/main/java/com/ruigoncalo/romeo/ui/SplashActivity.java
+++ app/src/main/java/com/ruigoncalo/romeo/ui/SplashActivity.java
@@ -1,7 +1,43 @@
 package com.ruigoncalo.romeo.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ruigoncalo.romeo.model.UserAuth;
+import com.ruigoncalo.romeo.providers.DataProvider;
+import com.ruigoncalo.romeo.ui.auth.AuthActivity;
+import com.ruigoncalo.romeo.ui.streams.StreamsActivity;
+
 /**
  * Created by ruigoncalo on 28/02/16.
  */
-public class SplashActivity {
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        navigateToNextScreen();
+    }
+
+    private void navigateToNextScreen(){
+        UserAuth userAuth = DataProvider.getInstance().getUserAuth(this);
+        if(userAuth.isValid()){
+            Intent intent = new Intent(this, StreamsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, AuthActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+    }
+
 }
+
