842ca8218f7b63b30bb8b37c7e884d9024e6c97b
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2016-03-14 23:53:20 +0000

    Refine some AudioProvider methods

--- app/src/main/java/com/ruigoncalo/romeo/providers/AudioProvider.java
+++ app/src/main/java/com/ruigoncalo/romeo/providers/AudioProvider.java
@@ -7,7 +7,6 @@ import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.net.Uri;
-import android.support.annotation.Nullable;
 
 import java.io.File;
 import java.io.FileOutputStream;
